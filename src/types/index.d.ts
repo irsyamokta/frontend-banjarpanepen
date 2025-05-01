aa499d6618cce489a57a2a00c09cd85c0e39d7d2
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-05-01 12:27:27 +0700

    Update : New interface

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -24,3 +24,33 @@ export interface IAuthContextType {
     logout: () => Promise<void>;
     checkAuth: () => Promise<void>;
 }
+
+export interface IArticlePayload {
+    title: string;
+    content: string;
+    writer: string;
+    thumbnail: string;
+}
+
+export interface IEventPayload {
+    title: string;
+    description: string;
+    date: string;
+    time: string;
+    place: string;
+    price: string;
+    thumbnail: string;
+}
+
+export interface IPackagePayload {
+    title: string;
+    price: string;
+    benefit: string;
+    thumbnail: string;
+}
+
+export interface IGalleryPayload {
+    title: string;
+    caption: string;
+    image: string;
+}
