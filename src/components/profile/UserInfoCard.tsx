b9019aca3f03efb3c115fa26abf0ef20a59947e9
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-05-05 02:52:04 +0700

    Update : User info card

--- src/components/profile/UserInfoCard.tsx
+++ src/components/profile/UserInfoCard.tsx
@@ -1,9 +1,7 @@
 import { useAuth } from "../../context/AuthContext";
-import { formatDate } from "../../utils/dateFormatter";
 
 export default function UserInfoCard() {
   const { user } = useAuth();
-  const formattedBirthDate = formatDate(user.birthDate);
 
   return (
     <div className="p-5 border border-gray-200 rounded-2xl dark:border-gray-800 lg:p-6">
@@ -16,7 +14,7 @@ export default function UserInfoCard() {
           <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-7 2xl:gap-x-32">
             <div>
               <p className="mb-2 text-xs leading-normal text-gray-500 dark:text-gray-400">
-                Full Name
+                Nama Lengkap
               </p>
               <p className="text-sm font-medium text-gray-800 dark:text-white/90">
                 {user.name}
@@ -25,28 +23,10 @@ export default function UserInfoCard() {
 
             <div>
               <p className="mb-2 text-xs leading-normal text-gray-500 dark:text-gray-400">
-                Email address
+                Email
               </p>
               <p className="text-sm font-medium text-gray-800 dark:text-white/90">
                 {user.email}
-              </p>
-            </div>
-
-            <div>
-              <p className="mb-2 text-xs leading-normal text-gray-500 dark:text-gray-400">
-                Gender
-              </p>
-              <p className="text-sm font-medium text-gray-800 dark:text-white/90">
-                {user.gender}
-              </p>
-            </div>
-
-            <div>
-              <p className="mb-2 text-xs leading-normal text-gray-500 dark:text-gray-400">
-                Birth Date
-              </p>
-              <p className="text-sm font-medium text-gray-800 dark:text-white/90">
-                {formattedBirthDate}
               </p>
             </div>
           </div>
