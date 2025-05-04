adf99da2e2529c82b25a96c5cc1b3b0e4c0f5085
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-05-05 02:56:03 +0700

    Add : Tour service

---
+++ src/services/tourService.ts
@@ -0,0 +1,35 @@
+import api from "../api";
+
+export const getTours = async () => {
+    const response = await api.get("/tour/all-tour", {
+        withCredentials: true,
+    });
+    return response.data;
+};
+
+export const createTour = async (payload: any) => {
+    const response = await api.post("/tour/create-tour", payload, {
+        withCredentials: true,
+        headers: {
+            "Content-Type": "multipart/form-data",
+        },
+    });
+    return response.data;
+};
+
+export const updateTour = async (id: string, payload: any) => {
+    const response = await api.patch(`/tour/update-tour/${id}`, payload, {
+        withCredentials: true,
+        headers: {
+            "Content-Type": "multipart/form-data",
+        },
+    });
+    return response.data;
+};
+
+export const deleteTour = async (id: string) => {
+    const response = await api.delete(`/tour/delete-tour/${id}`, {
+        withCredentials: true,
+    });
+    return response.data;
+};
