df265a9b1e6ba96f7e898d1a1e5083a84f9ec293
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-05-05 02:58:16 +0700

    Update : Event validator

--- src/utils/validator/eventValidator.ts
+++ src/utils/validator/eventValidator.ts
@@ -1,14 +1,19 @@
 import { z } from "zod";
 
+const timePattern = /^([01]\d|2[0-3]):([0-5]\d)$/;
+
 export const eventSchema = z.object({
     title: z.string()
+        .nonempty({ message: "Judul tidak boleh kosong!" })
         .min(3, { message: "Judul minimal harus 3 karakter!" })
         .max(100, { message: "Judul maksimal 100 karakter!" }),
 
     description: z.string()
+        .nonempty({ message: "Deskripsi tidak boleh kosong!" })
         .min(3, { message: "Deskripsi minimal harus 3 karakter!" }),
 
     date: z.string()
+        .nonempty({ message: "Tanggal tidak boleh kosong!" })
         .refine(
             (val) => {
                 const now = new Date();
@@ -19,15 +24,15 @@ export const eventSchema = z.object({
         ),
 
     time: z.string()
-        .regex(/^([0-1][0-9]|2[0-3]):[0-5][0-9]$/, {
-            message: "Waktu harus dalam format HH:mm (24 jam)!"
-        }),
+        .nonempty({ message: "Jam mulai tidak boleh kosong!" })
+        .regex(timePattern, { message: "Format jam mulai harus HH:mm (contoh: 08:00)" }),
 
     place: z.string()
+        .nonempty({ message: "Tempat tidak boleh kosong!" })
         .min(3, { message: "Tempat minimal harus 3 karakter!" }),
 
     price: z
         .number({ invalid_type_error: "Harga harus berupa angka!" })
-        .min(3, { message: "Harga minimal adalah 3!" })
+        .min(10000, { message: "Harga minimal adalah 10000!" })
         .optional()
 });
