716f768178d2213ba86094636cca9f6bb1e64a5a
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-05-05 02:54:56 +0700

    Update : Not found

--- src/pages/Errors/NotFound.tsx
+++ src/pages/Errors/NotFound.tsx
@@ -1,42 +1,30 @@
-import GridShape from "../../components/common/GridShape";
-import { Link } from "react-router";
+import { Link } from "react-router-dom";
 import PageMeta from "../../components/common/PageMeta";
 
 export default function NotFound() {
   return (
     <>
       <PageMeta
-        title="React.js 404 Dashboard | TailAdmin - React.js Admin Dashboard Template"
-        description="This is React.js 404 Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
+        title="Not Found"
+        description="Halaman tidak ditemukan"
       />
-      <div className="relative flex flex-col items-center justify-center min-h-screen p-6 overflow-hidden z-1">
-        <GridShape />
-        <div className="mx-auto w-full max-w-[242px] text-center sm:max-w-[472px]">
-          <h1 className="mb-8 font-bold text-gray-800 text-title-md dark:text-white/90 xl:text-title-2xl">
-            ERROR
-          </h1>
-
-          <img src="/images/error/404.svg" alt="404" className="dark:hidden" />
-          <img
-            src="/images/error/404-dark.svg"
-            alt="404"
-            className="hidden dark:block"
-          />
-
-          <p className="mt-10 mb-6 text-base text-gray-700 dark:text-gray-400 sm:text-lg">
-            We can’t seem to find the page you are looking for!
+      <div className="flex flex-col items-center justify-center p-6 overflow-hidden z-1 h-screen">
+        <div className="mx-auto w-full max-w-[420px] text-center">
+          <img src="/src/assets/img/img-404.png" alt="404" className="-mt-20" />
+          <p className="mb-6 text-base text-gray-700 dark:text-gray-400 sm:text-lg">
+            Maaf, halaman yang Anda cari tidak ditemukan.
           </p>
 
           <Link
             to="/"
-            className="inline-flex items-center justify-center rounded-lg border border-gray-300 bg-white px-5 py-3.5 text-sm font-medium text-gray-700 shadow-theme-xs hover:bg-gray-50 hover:text-gray-800 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.03] dark:hover:text-gray-200"
+            className="inline-flex items-center justify-center rounded-lg border border-gray-300 bg-primary px-5 py-3.5 text-sm font-medium text-white shadow-theme-xs hover:bg-primary/80 hover:text-white dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.03] dark:hover:text-gray-200"
           >
-            Back to Home Page
+            Kembali ke Beranda
           </Link>
         </div>
         {/* <!-- Footer --> */}
-        <p className="absolute text-sm text-center text-gray-500 -translate-x-1/2 bottom-6 left-1/2 dark:text-gray-400">
-          &copy; {new Date().getFullYear()} - TailAdmin
+        <p className="text-sm text-center text-gray-500 dark:text-gray-400 mt-10">
+          &copy; {new Date().getFullYear()} - Banjarpanepen
         </p>
       </div>
     </>
