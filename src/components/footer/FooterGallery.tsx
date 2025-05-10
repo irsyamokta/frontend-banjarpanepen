7672752ceee956938562ea59ba7b03cdeab69d73
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-05-10 19:41:25 +0700

    Update : Footer gallery

--- src/components/footer/FooterGallery.tsx
+++ src/components/footer/FooterGallery.tsx
@@ -1,17 +1,21 @@
-import galleryImage1 from "../../assets/img/img-galeri-1.png";
-import galleryImage2 from "../../assets/img/img-galeri-2.png";
-
-const IMAGES = [galleryImage1, galleryImage2, galleryImage2, galleryImage1];
+import useSWR from "swr";
+import { getGalleries } from "../../services/galleryService";
 
 function FooterGallery() {
+    const { data: response } = useSWR("footerGallery", getGalleries);
+
+    const sortedImages = response?.data
+        ?.sort((a: any, b: any) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime())
+        .slice(0, 4);
+
     return (
         <div className="text-center justify-center md:justify-self-end md:text-left">
             <h4 className="text-lg md:text-xl text-gray-100 font-bold mb-4">Galeri</h4>
             <div className="grid grid-cols-2 gap-4">
-                {IMAGES.map((image, index) => (
+                {sortedImages?.map((image: any, index: number) => (
                     <div key={index} className="overflow-hidden">
                         <img
-                            src={image}
+                            src={image.image}
                             alt={`Gallery Image ${index + 1}`}
                             className="w-full object-cover aspect-5/4 rounded-xl"
                         />
